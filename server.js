7a118a0e6337aa17f2f54ad2287225671d52c21d
SarthoraelEW <sarthoraelew@gmail.com>
2021-11-23 15:21:55 +0100

    add cors + end of backend v0

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-const messageRoutes = require('./routes/message.routes');
-const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const messageRoutes = require("./routes/message.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 // jwt
-app.get('*', checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id);
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
 });
 
 // routes
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/message', messageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
